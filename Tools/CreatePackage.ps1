adcdddf45e24e349970334ddf449636f5dac8ccb
Keiho Sakapon <git@saka-pon.net>
2016-12-09 18:25:37 +0900

    Add tool to create package

---
+++ Tools/CreatePackage.ps1
@@ -0,0 +1,15 @@
+﻿# $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+.\IncrementVersion-cs.ps1 ..\KLibrary3\Reactive
+
+$slnFilePath = "..\KLibrary3\KLibrary3.sln"
+& $msbuild $slnFilePath /p:Configuration=Release /t:Clean
+& $msbuild $slnFilePath /p:Configuration=Release /t:Rebuild
+
+cd ..\KLibrary3\Reactive
+.\NuGetPackup.exe
+
+ni ..\..\Published -type directory -Force
+move *.nupkg ..\..\Published -Force
+explorer ..\..\Published
